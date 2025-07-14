fe01e2d821775b4ae1b5ea85f6280ade81a083fb
Prabhath Dill <145959457+Dill1027@users.noreply.github.com>
2025-07-14 16:56:34 +0530

    Force rebuild: Add timestamp comment to trigger new deployment
    - Frontend deployment not picking up API URL fixes
    - Adding comment to force new build with corrected API endpoints
    - Local build confirmed to have correct /api paths

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Force rebuild with API URL fixes - $(date)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
